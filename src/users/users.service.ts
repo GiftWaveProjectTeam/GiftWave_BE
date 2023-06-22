4d6b0375997dae37d566e79c3e7ad522009b82fc
wndudtjd <dudtjdwn1575@naver.com>
2023-06-22 16:20:22 +0900

    [FIX] bcrypt라이브러리를 사용 비밀번호 암호화

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,7 @@ import { Users } from "src/entities/Users.entity";
 import { Repository } from "typeorm";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UserRole, UserStatus, UserType } from "./users.enum";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class UsersService {
@@ -26,9 +27,17 @@ export class UsersService {
             throw new BadRequestException('이미 존재하는 닉네임 입니다.')
         }
 
+        const userPhoneNumber = await this.userRepository.findOne({ where: {phone_number: phone_number}})
+        if(userPhoneNumber) {
+            throw new BadRequestException('이미 존재하는 전화번호 입니다.')
+        }
+
+        const salt = await bcrypt.genSalt()
+        const hashPW = await bcrypt.hash(password, salt)
+
         const result = this.userRepository.create({
             user,
-            password,
+            password: hashPW,
             phone_number,
             nickname,
             gender,
